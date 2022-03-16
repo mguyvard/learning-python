d5b1eef5d327c85865ffe20256091891b9cd78c4
mguyvard <guyvard@gmail.com>
2022-03-16 14:56:47 -0400

    added some .txt files

---
+++ count_words.py
@@ -0,0 +1,18 @@
+
+def count_words(filename):
+    """Count the number of words in a text file"""
+    try:
+        with open(file, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+
+filename = ['alice.txt', 'siddhartha.txt.', 'moby_dick.txt', 'little_women.txt']
+
+for file in filename:        
+    count_words(file)    
+  
